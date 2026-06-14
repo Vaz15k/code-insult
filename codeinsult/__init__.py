2fcc9aa2b13a6d79ba28e0e5424376b8c9fb401a
Vaz15K <keveneduardobilibio@hotmail.com>
2026-06-14 00:26:58 -0400

    feat: Import pt_br provider
    - We need to import the module, even if it's not "used".
    
    `ValueError: Idioma 'pt_br' não encontrado. Idiomas disponíveis: []. Use register_provider() para adicionar um novo idioma.`

--- codeinsult/__init__.py
+++ codeinsult/__init__.py
@@ -1,4 +1,5 @@
 # codeinsult/__init__.py
+import codeinsult.messages.pt_br  # noqa: F401, F811
 from codeinsult.__version__ import __version__
 from codeinsult.CodeInsult import insult, random_insult, set_defaults
 from codeinsult.config import InsultLevel
